272972f6fe088ef9c1e7bc3414cad0d98664ce7f
pengliang <624151713@qq.com>
2023-08-13 01:02:18 +0800

    feat: 大屏布局设计

--- src/components/BorderBox.vue
+++ src/components/BorderBox.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="r-border-box">
-    <slot></slot>
+  <div class="r-border-container flex flex-col">
+    <div class="r-border-title">{{ title }}</div>
+    <div class="r-border-body flex-1">
+      <slot></slot>
+    </div>
   </div>
 </template>
+
+<script setup>
+defineProps({
+  title: String
+})
+</script>
+
+<style lang="scss" scoped></style>
--- src/layout/BigScreenLayout.vue
+++ src/layout/BigScreenLayout.vue
@@ -8,13 +8,32 @@
     <div class="r-big-screen-container h-full flex flex-col">
       <div class="r-big-screen-header text-8xl text-center border">我是标题</div>
       <div class="r-big-screen-body flex-1">
-        <BorderBox class="r-big-screen-box border" v-for="box in 9" :key="box"> 22</BorderBox>
+        <BorderBox class="border" title="流水趋势">
+          <FlowingTrend />
+        </BorderBox>
+        <BorderBox class="border" title="大区">
+          <Regional />
+        </BorderBox>
+        <BorderBox class="border col-start-3 col-end-[span_2]" title="销售组">
+          <SalesTeam />
+        </BorderBox>
+        <BorderBox class="border" title="销售支持">
+          <SalesSupport />
+        </BorderBox>
+        <BorderBox class="border col-start-2 col-end-[span_3]" title="销售个人">
+          <Sales />
+        </BorderBox>
       </div>
     </div>
   </v-scale-screen>
 </template>
 
 <script setup>
+import Sales from '@/module/Sales.vue'
+import SalesSupport from '@/module/SalesSupport.vue'
+import SalesTeam from '@/module/SalesTeam.vue'
+import Regional from '@/module/Regional.vue'
+import FlowingTrend from '@/module/FlowingTrend.vue'
 import BorderBox from '@/components/BorderBox.vue'
 import VScaleScreen from 'v-scale-screen'
 import { reactive } from 'vue'
@@ -34,8 +53,11 @@ wh.w = rate * w
 <style lang="scss" scoped>
 .r-big-screen-body {
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(3, 1fr);
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(2, 1fr);
   grid-gap: 10px;
+}
+.kk {
+  grid-column: 3 / span 2;
 }
 </style>
